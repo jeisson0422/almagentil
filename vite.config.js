10f54485509cb24e51a38ad00857ed19d96ff262
Jeisson <50535334+jeisson0422@users.noreply.github.com>
2026-02-23 22:13:58 -0600

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: './', // Esto asegura que los recursos se carguen de manera relativa
     plugins: [
         tailwindcss(),
     ],
