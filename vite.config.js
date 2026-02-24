63d1bdb16bd184085b91d68329b5cf4647dd26e3
Jeisson <50535334+jeisson0422@users.noreply.github.com>
2026-02-23 22:36:04 -0600

    fix: setup multiple html entry points in vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: './', // Esto asegura que los recursos se carguen de manera relativa
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                programs: resolve(__dirname, 'programs.html'),
+                donate: resolve(__dirname, 'donate.html')
+            }
+        }
+    }
 });
